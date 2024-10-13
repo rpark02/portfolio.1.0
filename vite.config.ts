f3bac769637c3059dbab093a2e978c07f60b1457
rpark02 <78119104+rpark02@users.noreply.github.com>
2024-10-13 16:48:28 -0700

    update vite.config to have all correct assets

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from "path";
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -10,7 +9,16 @@ export default defineConfig({
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
-})
+    outDir: 'dist',          // Output directory
+    assetsDir: 'assets',     // Subdirectory for assets
+  },
+  assetsInclude: [
+    '**/*.ttf',  // Include .ttf font files
+    '**/*.otf',  // Include .otf font files
+    '**/*.png',  // Include .png image files
+    '**/*.jpg',  // Include .jpg image files
+    '**/*.jpeg', // Include .jpeg image files
+    '**/*.gif',  // Include .gif image files
+    '**/*.svg'   // Include .svg image files
+  ],
+});
